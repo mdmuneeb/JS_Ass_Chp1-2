79e8a0760e35e74b1bde5a34d360937d0468f0eb
mdmuneeb <muneebzafar999@gmail.com>
2023-09-01 20:38:31 +0500

    Completed

---
+++ Assignment 1/app.js
@@ -0,0 +1,66 @@
+// ? chapter1
+
+// ! Question 1
+
+// alert("Welcome to our website");    
+
+// ! Question 2
+// alert("Error! Please enter a valid password.");
+
+// ! Question 3
+// alert("Welcome to JS LAnd... \nHappy Coding")
+
+// ! Question 4
+// alert("Welcome to JS Land")
+// alert("Happy Coding!\nPrevent this page from creating additional dialogs")
+
+
+// ! Question 5
+// console.log("Hello... I can run JS through my web browser's console")
+
+
+
+
+
+
+// ? chapter1
+
+// ! Question 1
+// var username = "Muneeb";
+
+// ! Question 2
+// var myName = "Muhammad Muneeb Zafar";
+
+// ! Question 3
+// var message = "Hello World";
+// alert(message)
+
+// ! Question 4
+// var name = "Jhone Doe"
+// var age = 15
+// var certification = "Certified Mobile Application Development"
+
+// alert(name)
+// alert(age+"years old")
+// alert(certification)
+
+// ! Question 5
+// var variable = "PIZZA"
+// alert(variable+"\n"+variable.substring(0,4)+"\n"+variable.substring(0,3)+"\n"+variable.substring(0,2)+"\n"+variable.substring(0,1))
+
+// ! Question 6
+// var email = "example@example.com"
+// alert("Your email address is "+email)
+
+// ! Question 7
+// var variable = "A smarter way to learn JavaScript"
+// alert("I am trying to learn from the book "+variable) 
+
+// ! Question 8
+// let mypara = document.querySelector("div");
+// mypara.innerHTML = "Yeah! I can write HTML content through JavaScript"
+
+// ! Question 9
+// let mypara = document.querySelector("div");
+// mypara.innerHTML ="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
